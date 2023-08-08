32a479007c6bc1728a5e70145d8c6e760db51a83
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2023-08-08 21:57:07 +0200

    Attempting to fix borrow issue

--- src/view/update.rs
+++ src/view/update.rs
@@ -1,4 +1,5 @@
-use super::{View, ExtentUpdate};
+use super::{View, ExtentUpdate, UpdateFlags};
+use std::iter;
 
 /// Struct for allowing external users to update views
 pub struct ViewUpdater<'a> {
@@ -9,25 +10,21 @@ pub struct ViewUpdater<'a> {
 }
 
 impl<'a> ViewUpdater<'a> {
-    pub fn get_extent_updater(&mut self) -> ExtentUpdater<'a> {
-        let view = &mut self;
-        let extent = &mut self.view.extent.update_info;
-
-        ExtentUpdater { view, extent }
+    pub fn get_extent_update_info(&'a mut self) -> &'a mut ExtentUpdate {
+        &mut self.view.extent.update_info
     }
 
-    fn update_extent(&mut self) {
-        todo!();
-    }
-}
+    fn set_update_child(&mut self) {
+        // If flag was not already set, set it and for it's parent
+        if !self.view.update_flags.contains(UpdateFlags::UPDATE_EXTENT_CHILD) {
+            self.view.update_flags.extend(UpdateFlags::UPDATE_EXTENT_CHILD);
+            match self.parent.as_mut() {
+                // If there is a parent pass up the information
+                Some(parent) => parent.set_update_child(),
 
-pub struct ExtentUpdater<'a> {
-    view: &'a mut ViewUpdater<'a>,
-    pub extent: &'a mut ExtentUpdate,
-}
-
-impl<'a> Drop for ExtentUpdater<'a> {
-    fn drop(&mut self) {
-        self.view.update_extent();
+                // Do nothing if it is the root
+                None => (),
+            }
+        }
     }
 }
